72320ce314a18444c7386b2a0ae1ec5ba96630ab
Ahnaf Inkiad <ahnaf_inkiad@hks.harvard.edu>
2023-12-04 13:55:00 -0500

    modified some changes

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 const axios = require('axios');
 const cors = require('cors');
+const accessToken = 'heA-JOx6C-MuB8qN0nsRL064udrcg--WRjxpQL11pvzw1FSS7zfccWch_r80NjAm0nvgLmpWLjfDDWYVdSO8gA'; // Ideally fetched dynamically
+
 
 
 const getServiceNowIncident = async (incidentNumber) => {
    const apiUrl = `https://harvardtest.service-now.com/api/now/table/incident?sysparm_limit=1&number=${incidentNumber}`;
-   const accessToken = 'pjaCuhzu6ZRLMl-H4MI6tiqPDhhyJ8gVQmSto6BiRpUUm38tpCaPj8eawofUangIVGEk3idbIYdMITN_u6xEQA'; // Ideally fetched dynamically
+
 
    try {
        const response = await axios.get(apiUrl, {
@@ -17,7 +19,18 @@ const getServiceNowIncident = async (incidentNumber) => {
                'Content-Type': 'application/json'
            }
        });
-       return response.data.result; // Array of incidents
+       console.log("Direct API Response:", response.data); // Log direct API response
+       
+    
+    const incidentData = response.data.result[0]; // Assuming result is an array and you need the first object
+    console.log("Incident Data:", incidentData);
+    
+    if (!incidentData.assigned_to || !incidentData.assigned_to.value) {
+        console.error("Assigned to information is missing in the incident.");
+        return incidentData; // Or handle this scenario appropriately
+    }
+    
+       return response.data.result; 
    } catch (error) {
        console.error('Error fetching incident:', error);
        throw error;
@@ -26,22 +39,64 @@ const getServiceNowIncident = async (incidentNumber) => {
 
 
 const getServiceNowUser = async (userId) => {
-   // Construct the URL to the user endpoint
-   const userApiUrl = `https://harvardtest.service-now.com/api/now/table/sys_user/${userId}`;
- 
-   try {
-     const response = await axios.get(userApiUrl, {
-       headers: {
-         'Authorization': `Bearer ${accessToken}`,
-         'Content-Type': 'application/json'
-       }
-     });
-     return response.data.result;
-   } catch (error) {
-     console.error('Error fetching user:', error);
-     throw error;
-   }
- };
+    console.log("getServiceNowUser called with userId:", userId);
+
+    const userApiUrl = `https://harvardtest.service-now.com/api/now/table/sys_user?sysparm_limit=1&sys_id=${userId}`;
+
+    try {
+        const response = await axios.get(userApiUrl, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.data.result[0] || response.data.result[0].length === 0) {
+            console.error("No incidents found in the response.");
+            return null; // Or handle this scenario appropriately
+        }
+        
+
+        // Check if the response has any results and extract the name
+        if (response.data.result[0]) {
+            console.log("Name of user: " + response.data.result[0].name)
+            return response.data.result[0].name; // Return the name of the first user in the results
+        } else {
+            return 'Unknown'; // Default name if user is not found or result is empty
+        }
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        throw error;
+    }
+};
+
+
+
+const getServiceNowUserIdByName = async (technicianName) => {
+    const userApiUrl = `https://harvardtest.service-now.com/api/now/table/sys_user?sysparm_limit=1&name=${technicianName}`;
+
+    try {
+        const response = await axios.get(userApiUrl, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.data.result[0]) {
+            return response.data.result[0].sys_id;
+        } else {
+            return null; // User not found
+        }
+    } catch (error) {
+        console.error('Error fetching user:', error);
+        throw error;
+    }
+};
+
+
+
+
 
 app.use(express.json());
 app.use(cors());
@@ -84,21 +139,65 @@ app.get('/oauth/callback', async (req, res) => {
    }
 });
 
-
-
-
 app.get('/api/incidents', async (req, res) => {
-   try {
-       const incident = await getServiceNowIncident('INC05091001'); // Example incident number
-       if (incident.assigned_to && incident.assigned_to.value) {
-         const assignedUser = await getServiceNowUser(incident.assigned_to.value);
-         incident.assigned_to_name = assignedUser.name; // Assuming 'name' is the field you want
-       }
-       res.json(incident);
-   } catch (error) {
-       res.status(500).json({ message: error.message });
-   }
+    try {
+        const incident = await getServiceNowIncident('INC05184397');
+        console.log("Fetched Incident:", incident); // Log after fetching
+        console.log("Incident assigned value", incident[0].assigned_to.value);
+
+        if (incident[0].assigned_to && incident[0].assigned_to.value) {
+            const assignedUserName = await getServiceNowUser(incident[0].assigned_to.value);
+            console.log("Assigned user name:", assignedUserName);
+            incident[0].assigned_to_name = assignedUserName;
+            console.log("Updated Incident data:", incident);
+        }
+        res.json(incident);
+    } catch (error) {
+        console.error("Error in /api/incidents:", error);
+        res.status(500).json({ message: error.message });
+    }
 });
+
+
+app.put('/api/incidents/:incidentSysId', async (req, res) => {
+    const incidentSysId = req.params.incidentSysId;
+    const technicianName = req.body.technicianName;
+
+    try {
+        const technicianSysId = await getServiceNowUserIdByName(technicianName);
+        if (!technicianSysId) {
+            return res.status(404).send('Technician not found');
+        }
+
+        // Update the incident
+        await axios.put(`https://harvardtest.service-now.com/api/now/table/incident/${incidentSysId}`, {
+            assigned_to: technicianSysId
+        }, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        // Fetch the updated incident data
+        const updatedIncidentResponse = await axios.get(`https://harvardtest.service-now.com/api/now/table/incident/${incidentSysId}`, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        // Return the updated incident data
+        res.json(updatedIncidentResponse.data);
+    } catch (error) {
+        console.error('Error updating incident:', error);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+
+
 
 app.listen(port, () => {
    console.log(`Server running on http://localhost:${port}`);
